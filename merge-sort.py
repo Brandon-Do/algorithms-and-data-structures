5ed79987d08ec571ef25f6f3300746e87ff6d693
Brandon Do <brandondo64@gmail.com>
2020-08-08 18:46:30 -0700

    Implement merge sort on an array of random ints

---
+++ merge-sort.py
@@ -0,0 +1,46 @@
+import random
+
+
+def get_random_ints(count: int, lower_bound = 0, upper_bound = 10000) -> list:
+  return [random.randint(lower_bound, upper_bound) for _ in range(count)]
+
+
+def merge_sort(nums: list) -> list:
+  if len(nums) <= 1:
+    return nums
+
+  mid = len(nums) // 2
+  first_half = merge_sort(nums[:mid])
+  second_half = merge_sort(nums[mid:])
+
+  return merge(first_half, second_half)
+
+
+def merge(first_half: list, second_half: list) -> list:
+  if not (first_half or second_half): 
+    return [] # both lists are empty
+
+  results = []
+  i, j = 0, 0
+
+  while i < len(first_half) and j < len(second_half):
+    if first_half[i] < second_half[j]:
+      results.append(first_half[i])
+      i += 1
+    else:
+      results.append(second_half[j])
+      j += 1
+  
+  if i < len(first_half):
+    results += first_half[i:]
+  elif j < len(second_half):
+    results += second_half[j:]
+  
+  return results
+
+
+if __name__ == "__main__":
+  numbers = get_random_ints(20)
+  print(numbers)
+  sorted_numbers = merge_sort(numbers)
+  print(sorted_numbers)
