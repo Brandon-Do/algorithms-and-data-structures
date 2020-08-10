242d3065c14c8551589304ce2c140abe0f4718e0
Brandon Do <brandondo64@gmail.com>
2020-08-10 11:42:07 -0700

    Create common methods script

--- merge-sort.py
+++ merge-sort.py
@@ -1,10 +1,3 @@
-import random
-
-
-def get_random_ints(count: int, lower_bound = 0, upper_bound = 10000) -> list:
-  return [random.randint(lower_bound, upper_bound) for _ in range(count)]
-
-
 """
   Implemented merge sort here
   Time: O(nlogn) -- scan the array each split
