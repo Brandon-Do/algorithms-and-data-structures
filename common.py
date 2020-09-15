6b785f7551014008b617d8e87858140a43a4babb
Brandon Do <brandondo64@gmail.com>
2020-09-14 22:04:28 -0700

    Use deque min queue to keep min value while iterating over the array

--- common.py
+++ common.py
@@ -1,9 +1,9 @@
-import random
-
+from random import randint
+from time import time
 
 def get_random_ints(count: int, lower_bound = 0, upper_bound = 99) -> list:
-  return [random.randint(lower_bound, upper_bound) for _ in range(count)]
+  return [randint(lower_bound, upper_bound) for _ in range(count)]
 
 
 if __name__ == "__main__":
-    pass
+  pass
---
+++ min_deque.py
@@ -0,0 +1,14 @@
+from collections import deque
+from common import get_random_ints
+
+def main():
+  min_deque = deque()
+  for number in get_random_ints(20, 0, 10):
+    while min_deque and number < min_deque[-1]:
+      print('Removed:', min_deque.pop(), 'from min deque')
+    min_deque.append(number)
+    print(min_deque)
+  
+
+if __name__ == "__main__":
+    main()
